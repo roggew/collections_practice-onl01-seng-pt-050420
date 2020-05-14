bee05ffeed47221761f80285826007a698b6aaeb
William Rogge <williamroggeiv@gmail.com>
2020-05-14 21:24:41 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+  
+  def reverse_array(array)
+    array.reverse
+  end
+  
+  def kesha_maker(array)
+    array.each do |x|
+      x[2] = "$"
+    end
+  end
+  
+  def find_a(array)
+    array.select{|x| x.start_with? "a"}
+  end
+  
+  def sum_array(array)
+   array.inject{|sum, x| sum + x}
+  end 
+  
+  def add_s(array)
+    array.collect do |x|
+      if x==array[1] 
+         x  
+      else x !=array[1]
+       x + "s"
+      end
+  end
+end
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
